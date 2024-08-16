a0141c42009a9daaae541f09bc2710d260ee78c2
Gune Ko <93659082+Koko1324@users.noreply.github.com>
2024-08-16 21:07:03 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 #인사말
-header_col1, header_col2, header_col3 = st.columns([1,2,1])
+header_col1, header_col2, header_col3 = st.columns([1,1,1])
 with header_col2:
     st.write("공부 시간 측정 도우미")
